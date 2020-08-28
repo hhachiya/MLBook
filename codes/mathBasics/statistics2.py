22d944d3a9a9f7eb31d04be206a2e5f2385b5929
Hirotaka Hachiya <hhachiya@wakayama-u.ac.jp>
2020-08-28 12:46:48 +0900

    replaced cov_nobias with cov_bias

--- codes/mathBasics/statistics2.py
+++ codes/mathBasics/statistics2.py
@@ -10,8 +10,8 @@ vars = np.var(X,axis=0)
 stds = np.std(X,axis=0)
 
 # Xを転置（変数×データ数）して，分散共分散行列の計算
-cov_bias = np.cov(X.T)
-cov_nobias = np.cov(X.T,bias=1)
+cov_nobias = np.cov(X.T)
+cov_bias = np.cov(X.T,bias=1)
 
 # 標準出力
 print(f"データX:\n{X}\n")
@@ -20,6 +20,6 @@ print(f"分散）身長:{vars[0]:.2f}，体重:{vars[1]:.2f}，胸囲:{vars[2]:.
 
 print(f"標準偏差）身長:{stds[0]:.2f}，体重:{stds[1]:.2f}，胸囲:{stds[2]:.2f}\n")
 
-print(f"分散共分散行列 バイアスあり:\n{cov_bias}\n")
-
 print(f"分散共分散行列 バイアスなし:\n{cov_nobias}\n")
+
+print(f"分散共分散行列 バイアスあり:\n{cov_bias}\n")
